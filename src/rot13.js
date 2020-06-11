3f9a28ce23c5740ac34cab8175749b52e2221f51
xing <xing@integral.io>
2020-06-10 23:23:00 -0400

    Implement ROT-13 algorithm

---
+++ src/rot13.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function charCodeOf(char) {
+    return char.charCodeAt(0);
+}
+
+function isBetween(charCode, begin, inclusiveEnd) {
+    return charCode >= charCodeOf(begin) && charCode <= charCodeOf(inclusiveEnd);
+}
+
+function isBetweenCaseInsensitive(charCode, begin, inclusiveEnd) {
+    return isBetween(charCode, begin.toLowerCase(), inclusiveEnd.toLowerCase())
+        || isBetween(charCode, begin.toUpperCase(), inclusiveEnd.toUpperCase());
+}
+
+function transform(char) {
+    const charCode = charCodeOf(char);
+    if (isBetweenCaseInsensitive(charCode, 'n', 'z')) return String.fromCharCode(charCode - 13);
+    if (isBetweenCaseInsensitive(charCode, 'a', 'm')) return String.fromCharCode(charCode + 13);
+    return char;
+}
+
+exports.transform = function (input) {
+    if (typeof input !== "string") throw new Error("Expect input to be a string");
+    return [...input].map(transform).join('');
+};
---
+++ src/rot13_test.js
@@ -0,0 +1,44 @@
+// Copyright Titanium I.T. LLC.
+'use strict';
+
+const assert = require('./assert');
+const rot13 = require('./rot13');
+
+describe('ROT-13', function () {
+
+	it('does nothing when input is empty', function () {
+		assert.equal(rot13.transform(''), '');
+	});
+
+	it('transforms lowercase letters', function () {
+		assert.equal(rot13.transform('abcdefghijklmnopqrstuvwxyz'), 'nopqrstuvwxyzabcdefghijklm');
+	});
+
+	it('transforms uppercase letters', function () {
+		assert.equal(rot13.transform('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'NOPQRSTUVWXYZABCDEFGHIJKLM');
+	});
+
+	it('does not transform emojis', function () {
+		assert.equal(rot13.transform('😁😃😱😰'), '😁😃😱😰');
+	});
+	
+	it('does not transform symbols', function () {
+		assert.equal(rot13.transform('`{@'), '`{@');
+	});
+
+	it('does not transform numbers', function () {
+		assert.equal(rot13.transform('1234567890'), '1234567890');
+	});
+
+	it('does not transform non-English letters', function () {
+		assert.equal(rot13.transform('åç¡™£åß∂ƒ'), 'åç¡™£åß∂ƒ');
+	});
+
+	it('fails fast when input is not a string', function () {
+		assert.throws(
+			() => rot13.transform(),
+			"Expect input to be a string"
+		);
+	});
+
+});
